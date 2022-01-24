c4bcd3289a64249625371a76383223c17edd4373
Fabrega Jocelyn <91054077+JocelynFabrega@users.noreply.github.com>
2022-01-24 15:54:51 +0100

    ajout pour cache le formulaire

--- squelette-projet-notes/js/notes.js
+++ squelette-projet-notes/js/notes.js
@@ -3,3 +3,10 @@ let aInfo = document.querySelector("#noteForm");
 a1.addEventListener("click", (event) => {
     aInfo.classList.remove("create_edit_note-hidden");
 });
+
+let valide = document.querySelector("#form_add_note_valid");
+
+valide.addEventListener("click", (event) => {
+    //TODO
+    aInfo.classList.add("create_edit_note-hidden");
+});
